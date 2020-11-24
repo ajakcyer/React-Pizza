075b55b9b708cc63bcd5034eedba54f5057a3318
ajakcyer <ajakcyer97@gmail.com>
2020-11-23 20:57:59 -0500

    done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,89 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+
+  state = {
+    pizzaId: "",
+    editPizzaValues: {
+      topping: "",
+      size: "",
+      vegetarian: null
+    },
+    pizzas: []
+  }
+
+  componentDidMount = () =>{
+    fetch("http://localhost:3000/pizzas")
+    .then(r=>r.json())
+    .then(data => this.setState(prevState => ({pizzas: data})))
+  }
+
+  pizzaToEdit = (pizzaObj) =>{
+    this.setState(prevState => ({editPizzaValues: {...prevState.editPizzaValues, topping: pizzaObj.topping, size: pizzaObj.size, vegetarian: pizzaObj.vegetarian}, pizzaId: pizzaObj.id}))
+    // console.log("in App", pizzaObj)
+  }
+
+  changeHandler = (e) =>{
+    e.persist()
+    console.log("hi from app", e.target.name, e.target.value)
+    this.setState(prevState=>({
+      editPizzaValues: {...prevState.editPizzaValues, [e.target.name]: e.target.value}
+    }))
+  }
+
+  veggieChangeHandler = (e) =>{
+    e.persist()
+    console.log("hi from app", e.target.value, e.target.checked)
+
+    if (e.target.value === "Vegetarian"){
+      this.setState(prevState=>({
+        editPizzaValues: {...prevState.editPizzaValues, [e.target.name]: e.target.checked}
+      }))
+      return
+    }
+
+    this.setState(prevState=>({
+      editPizzaValues: {...prevState.editPizzaValues, vegetarian: false}
+    }))
+  }
+
+  submitHandler = (pizzaObj) =>{
+    // console.log("in app", pizzaObj, this.state.pizzaId)
+    const pizzaObjConfig = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(pizzaObj)
+    }
+    fetch(`http://localhost:3000/pizzas/${this.state.pizzaId}`, pizzaObjConfig)
+    .then(r=>r.json())
+    .then(updatedPizza => {
+      let copiedArray = [...this.state.pizzas]
+
+      let thisPizza = copiedArray.findIndex(pizza => pizza.id === updatedPizza.id)
+      // console.log(thisPizza)
+
+      // let indexOfPizza = copiedArray.indexOf(thisPizza)
+
+      copiedArray[thisPizza] = updatedPizza
+
+      this.setState(prevState => ({
+        pizzas: copiedArray
+      }))
+    })
+    .catch(console.log)
+
+  }
+
   render() {
+    console.log("state in app:", this.state)
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm submitHandler={this.submitHandler} veggieChangeHandler={this.veggieChangeHandler} changeHandler={this.changeHandler} pizza={this.state.editPizzaValues} />
+        <PizzaList pizzas={this.state.pizzas} pizzaToEdit={this.pizzaToEdit} />
       </Fragment>
     );
   }
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import Pizza from '../components/Pizza'
 class PizzaList extends Component {
 
+  // state = {
+  //   pizzas: []
+  // }
+
+  renderPizzas = () =>{
+    return this.props.pizzas.map(pizzaObj => <Pizza pizzaToEdit={this.props.pizzaToEdit} key={pizzaObj.id} pizza={pizzaObj} />)
+  }
+
+  // componentDidMount = () =>{
+  //   fetch("http://localhost:3000/pizzas")
+  //   .then(r=>r.json())
+  //   .then(data => this.setState(prevState => ({pizzas: data})))
+  // }
+
   render() {
+    // console.log(this.state.pizzas)
     return (
       <table className="table table-striped">
         <thead>
@@ -14,7 +29,7 @@ class PizzaList extends Component {
           </tr>
         </thead>
         <tbody>
-          {
+          { this.props.pizzas.length === 0 ? null : this.renderPizzas()
             //render Pizza here
           }
         </tbody>
